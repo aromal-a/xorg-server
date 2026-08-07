d8f5394a2838b3584850ce8057f5b62dfb4666f2
Aromal A <82209054+aromal-a@users.noreply.github.com>
2026-08-07 15:10:18 +0530

    Update .dir-locals.el with new configurations

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,8 @@
 ((c-mode . ((c-basic-offset . 4) (indent-tabs-mode . nil))))
+{[BASIC-offset {off{$: certs}}]}[Indent : new ,  space-cloumn : <CMI>]
+
+CD-let : cmd
+Ci-let : Emmy
+
+
+Gram-awards : <Nation.wide : selections = ['Best-phones', Grammy_wide = 'close-effort' : high]>
